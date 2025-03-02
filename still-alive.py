7174d4bba4795df570f3b4b50a99fcd05a4ceaed
ivanp7 <ivanp7@mail.ru>
2025-03-02 22:53:22 +0300

    chore: follow libarchipelago changes

--- still-alive.py
+++ still-alive.py
@@ -23,8 +23,8 @@ TEXTURE_HEIGHT = FONT_HEIGHT * SCREEN_SIZE_Y
 
 NUM_THREADS = 4 # number of rendering threads
 
-WINDOW_WIDTH  = TEXTURE_WIDTH * 2
-WINDOW_HEIGHT = TEXTURE_HEIGHT * 2
+WINDOW_WIDTH_SCALE  = 2 # number of window pixels per texture pixels, can be fractional
+WINDOW_HEIGHT_SCALE = WINDOW_WIDTH_SCALE
 
 WINDOW_TITLE = "Still Alive"
 
@@ -44,36 +44,42 @@ app.watch_signal('SIGCONT') # the song handles Ctrl+Z
 app.load_library('libsdl', 'libarchi-sdl.so')     # load the plugin for SDL support
 app.load_library('still-alive', 'still-alive.so') # load the song plugin
 
-# Register interfaces
+# Register context interfaces
 app.register_interface('sdl_library', 'plugin_sdl_library_interface', 'libsdl')       # register the SDL library interface
 app.register_interface('sdl_window', 'plugin_sdl_window_context_interface', 'libsdl') # register the SDL window context interface
 app.register_interface('glados', 'glados_interface', 'still-alive')                   # register the song interface
+
+# Obtain context interfaces
+thread_group_interface = app.interface(archi.THREAD_GROUP_INTERFACE_KEY)
+sdl_library_interface = app.interface('sdl_library')
+sdl_window_interface = app.interface('sdl_window')
+glados_interface = app.interface('glados')
 
 # Obtain built-in contexts
 app_signal = app.context(archi.APP_SIGNAL_CONTEXT_KEY) # obtain the application signal management context
 app_fsm = app.context(archi.APP_FSM_CONTEXT_KEY) # obtain the application finite state machine
 
 # Create contexts
-thread_group = app.new_context(
-        'thread_group', 'thread_group', # a group of threads for multicore rendering
+thread_group = thread_group_interface( # a group of threads for multicore rendering
+        'thread_group',
         num_threads=archi.Value(c.c_size_t(NUM_THREADS), type=archi.VALUE_UINT)) # number of threads in the group
 
-sdl_library = app.new_context(
-        'sdl_library', 'sdl_library', # an auxiliary context helping with initializing/finalizing SDL library
-        audio=archi.Value(None), # initialize the 'audio' subsystem
-        video=archi.Value(None)) # initialize the 'video' subsystem
+sdl_library = sdl_library_interface( # an auxiliary context helping with initializing/finalizing SDL library
+        'sdl_library',
+        audio=archi.Value(None, type=archi.VALUE_TRUE), # initialize the 'audio' subsystem
+        video=archi.Value(None, type=archi.VALUE_TRUE)) # initialize the 'video' subsystem
 
 # NOTICE: sdl_library must be created once before any sdl_window to initialize SDL properly
 
-sdl_window = app.new_context(
-        'sdl_window', 'sdl_window', # a drawable window
+sdl_window = sdl_window_interface( # a drawable window
+        'sdl_window',
         texture_width=archi.Value(c.c_int(TEXTURE_WIDTH), type=archi.VALUE_SINT),   # window canvas width
         texture_height=archi.Value(c.c_int(TEXTURE_HEIGHT), type=archi.VALUE_SINT), # window canvas height
-        window_width=archi.Value(c.c_int(WINDOW_WIDTH), type=archi.VALUE_SINT),     # window frame width
-        window_height=archi.Value(c.c_int(WINDOW_HEIGHT), type=archi.VALUE_SINT),   # window frame height
+        window_width=archi.Value(c.c_float(WINDOW_WIDTH_SCALE), type=archi.VALUE_FLOAT),   # window frame width scale
+        window_height=archi.Value(c.c_float(WINDOW_HEIGHT_SCALE), type=archi.VALUE_FLOAT), # window frame height scale
         window_title=archi.Value(c.create_string_buffer(WINDOW_TITLE.encode()), type=archi.VALUE_STRING)) # window title
 
-glados = app.new_context('glados', 'glados') # the GLaDOS context (song context)
+glados = glados_interface('glados') # the GLaDOS context (song context)
 
 # Configure the GLaDOS context
 glados.signal_flags = app_signal.signal_flags # provide pointer to the signal flags to GLaDOS
